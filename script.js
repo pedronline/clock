5e0d4063cfa0b4f9ae92a72b62ee66520d870eca
pedro <pedromorais2@hotmail.com>
2019-10-21 11:29:54 +0100

    clock working

---
+++ script.js
@@ -0,0 +1,19 @@
+const hourHand = document.querySelector(".hour-hand");
+const minHand = document.querySelector(".min-hand");
+const secHand = document.querySelector(".sec-hand");
+
+const deg = num => (num / 60) * 360 + 90;
+
+const setDate = () => {
+  const now = new Date();
+  const sec = deg(now.getSeconds());
+  const min = deg(now.getMinutes());
+  const hour = deg(now.getHours()/60*12);
+  console.log(hour, min, sec);
+
+  secHand.style.transform = `rotate(${sec}deg)`;
+  minHand.style.transform = `rotate(${min}deg)`;
+  hourHand.style.transform = `rotate(${hour}deg)`;
+};
+
+setInterval(setDate, 1000);
